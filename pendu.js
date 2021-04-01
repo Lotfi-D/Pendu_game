2f3a6bcc01983ca127e915e00557767798f9f91f
Lotfi <lotfi.doucanef@epitech.eu>
2021-04-01 12:42:18 +0200

    [FIX] the bug when we click on nouvelle partie has been fixed

--- pendu.js
+++ pendu.js
@@ -4,8 +4,8 @@ let words = [
     'Paranoïa', 'Parcours', 'Pastiche', 'Question', 'Quetsche', 'Scarabée', 'Scorpion', 'Symptôme', 'Tabouret', 'Tomahawk', 'Toujours', 'Tourisme',
     'Triangle', 'Utopique', 'Zeppelin', 'Accordéon', 'Ascenseur', 'Ascension', 'Aseptiser', 'Autoroute', 'Avalanche', 'Balalaïka', 'Bilboquet', 'Bourricot',
     'Brillance', 'Cabriolet', 'Contrario', 'Cornemuse', 'Dangereux', 'Épluchage', 'Féodalité', 'Forteresse', 'Gondolier', 'Graphique', 'Horoscope', 'Intrépide',
-    'Klaxonner', 'Mascarade', 'Métaphore', 'Narrateur', 'Péripétie', 'Populaire', 'Printemps', 'Quémander', 'Tambourin', 'Vestiaire', 'Xylophone', 'Acrostiche', 
-    'Apocalypse','Attraction', 'Aventurier', 'Bouillotte', 'Citrouille', 'Controverse', 'Coquelicot', 'Dissimuler', 'Flibustier', 'Forestière', 'Grenouille',
+    'Klaxonner', 'Mascarade', 'Métaphore', 'Narrateur', 'Péripétie', 'Populaire', 'Printemps', 'Quémander', 'Tambourin', 'Vestiaire', 'Xylophone', 'Acrostiche',
+    'Apocalypse', 'Attraction', 'Aventurier', 'Bouillotte', 'Citrouille', 'Controverse', 'Coquelicot', 'Dissimuler', 'Flibustier', 'Forestière', 'Grenouille',
     'Impossible', 'Labyrinthe', 'Maharadjah', 'Prudemment', 'Quadriceps', 'Soliloquer', 'Subjective', 'Abriter', 'Ballast', 'Baryton', 'Bassine', 'Batavia',
     'Billard', 'Bretzel', 'Cithare', 'Chariot', 'Clairon', 'Corbeau', 'Cortège', 'Crapaud', 'Cymbale', 'Dentier', 'Djembé', 'Drapeau', 'Exemple',
     'Fourmis', 'Grandir', 'Iceberg', 'Javelot', 'Jockey', 'Journal', 'Journée', 'Jouxter', 'Losange', 'Macadam', 'Mondial', 'Notable', 'Oxygène',
@@ -13,11 +13,11 @@ let words = [
     'Tunique', 'Ukulélé', 'Vautour', 'Zozoter', 'Acajou', 'Agneau', 'Alarme', 'Ananas', 'Angora', 'Animal', 'Arcade', 'Aviron', 'Azimut', 'Babine',
     'Balade', 'Bonzaï', 'Basson', 'Billet', 'Bouche', 'Boucle', 'Bronze', 'Cabane', 'Caïman', 'Cloche', 'Chèque', 'Cirage', 'Coccyx', 'Crayon', 'Garage',
     'Gospel', 'Goulot', 'Gramme', 'Grelot', 'Guenon', 'Hochet', 'Hormis', 'Humour', 'Hurler', 'Jargon', 'Limite', 'Lionne', 'Menthe', 'Oiseau', 'Podium',
-    'Poulpe', 'Poumon', 'Puzzle', 'Quartz', 'Rapide', 'Séisme', 'Tétine', 'Tomate', 'Walabi', 'Whisky', 'Zipper', 'Accès', 'Aimer', 'Aloès', 'Assez', 'Avion', 
-    'Awalé', 'Balai', 'Banjo', 'Barbe', 'Bonne', 'Bruit', 'Buche', 'Cache', 'Capot', 'Carte', 'Chien', 'Crâne', 'Cycle', 'Ébène', 'Essai', 'Gifle', 'Honni', 'Jambe', 
-    'Koala', 'Livre', 'Lourd', 'Maman', 'Moult', 'Noeud', 'Ortie', 'Pêche', 'Poire', 'Pomme', 'Poste', 'Prune', 'Radar', 'Radis', 'Robot', 'Route', 'Rugby', 'Seuil', 
+    'Poulpe', 'Poumon', 'Puzzle', 'Quartz', 'Rapide', 'Séisme', 'Tétine', 'Tomate', 'Walabi', 'Whisky', 'Zipper', 'Accès', 'Aimer', 'Aloès', 'Assez', 'Avion',
+    'Awalé', 'Balai', 'Banjo', 'Barbe', 'Bonne', 'Bruit', 'Buche', 'Cache', 'Capot', 'Carte', 'Chien', 'Crâne', 'Cycle', 'Ébène', 'Essai', 'Gifle', 'Honni', 'Jambe',
+    'Koala', 'Livre', 'Lourd', 'Maman', 'Moult', 'Noeud', 'Ortie', 'Pêche', 'Poire', 'Pomme', 'Poste', 'Prune', 'Radar', 'Radis', 'Robot', 'Route', 'Rugby', 'Seuil',
     'Taupe', 'Tenue', 'Texte', 'Tyran', 'Usuel', 'Valse',
-    ];
+];
 
 let wordNow = '';
 let tableau = [];
@@ -34,6 +34,7 @@ function changeWord() {
 
     count = 0;
     error = 0;
+    finish = false;
 
     do {
         number = Math.floor(Math.random() * Math.floor(words.length));
@@ -54,66 +55,76 @@ function changeWord() {
 
     tableau.push(wordNow[wordNow.length - 1]);
     document.querySelector('#word').textContent = tableau.join(' ').toUpperCase();
-    console.log('number: ' + number, 'numberBefore:' + numberBefore)
+    //console.log('number: ' + number, 'numberBefore:' + numberBefore)
     numberBefore = number;
 };
 
 function letterToFind() {
-    let inputLetter = document.querySelector("#findWord").value;
-    let index = wordNow.indexOf(inputLetter);
+    try {
+        let inputLetter = document.querySelector("#findWord").value;
+        let index = wordNow.indexOf(inputLetter);
+        console.log('index : ', index);
+        console.log('partie finish', finish)
 
-    if (index !== (-1) && finish === false) {
-        for (i = 0; i < wordNow.length; i++) {
-            if (wordNow[i] === inputLetter) {
-                tableau.splice(i, 1, inputLetter);
+        if (index !== (-1) && finish === false) {
+            for (i = 0; i < wordNow.length; i++) {
+                if (wordNow[i] === inputLetter) {
+                    tableau.splice(i, 1, inputLetter);
+                }
+            }
+            document.querySelector('#word').textContent = tableau.join(' ').toUpperCase();
+            document.querySelector('#message').innerHTML = "<p class=' text-success fw-bold mt-3'> Bonne réponse : La lettre " + inputLetter.toUpperCase() + " fait bien partie du mot</p>";
+            if (count < 11) {
+                count++;
             }
         }
-        document.querySelector('#word').textContent = tableau.join(' ').toUpperCase();
-        document.querySelector('#message').innerHTML = "<p class=' text-success fw-bold mt-3'> Bonne réponse : La lettre " + inputLetter.toUpperCase() + " fait bien partie du mot</p>";
-        if (count < 10) {
-            count++;
-        }
-    }
-    if (index === (-1) && error < 8 && finish === false) {
-        if (error < 8 && count < 10) {
-            error++;
-            count++;
+        if (index === (-1) && error < 8 && finish === false) {
+            if (error < 8 && count < 11) {
+                error++;
+                count++;
+            }
+
+            let messageError;
+
+            if ((8 - error) === 1) {
+                messageError = 'Vous avez encore le droit à ' + (8 - error) + ' mauvaise réponse !';
+            } else {
+                messageError = 'Vous avez encore le droit à ' + (8 - error) + ' mauvaises réponses !';
+            }
+
+            document.querySelector('#message').innerHTML = "<p class='text-danger fw-bold mt-3'>Mauvaise réponse : La lettre " + inputLetter.toUpperCase() + " ne fait pas partie du mot</p>";
+            document.querySelector('#dessin').innerHTML = "<img src='" + error + ".png' class='img-fluid' style='width:15rem'><p class='mt-2'>" + messageError + "</p>";
         }
 
-        let messageError;
-
-        if ((8 - error) === 1) {
-            messageError = 'Vous avez encore le droit à ' + (8 - error) + ' mauvaise réponse !';
+        //message à afficher en fonction des coups et des erreurs    
+        if ((10 - count) === 1) {
+            document.querySelector('#messageRound').textContent = "Il vous reste " + (11 - count) + " coup à jouer";
         } else {
-            messageError = 'Vous avez encore le droit à ' + (8 - error) + ' mauvaises réponses !';
+            document.querySelector('#messageRound').textContent = "Il vous reste " + (11 - count) + " coups à jouer";
+        };
+
+        if (error === 8 || count === 11) {
+            document.querySelector('#messageRound').innerHTML = "<p class='text-danger fw-bold'>La partie est finie, Vous avez perdu ! Le mot était " + wordNow.join('').toUpperCase() + " </p>";
+            document.querySelector('#message').innerHTML = "";
+            finish = true;
+            // alert('Vous avez perdu la partie');
         }
 
-        document.querySelector('#message').innerHTML = "<p class='text-danger fw-bold mt-3'>Mauvaise réponse : La lettre " + inputLetter.toUpperCase() + " ne fait pas partie du mot</p>";
-        document.querySelector('#dessin').innerHTML = "<img src='" + error + ".png' class='img-fluid' style='width:15rem'><p class='mt-2'>" + messageError + "</p>";
+        if (tableau.toString() === wordNow.toString()) {
+            document.querySelector('#messageRound').innerHTML = "<p class='text-success fw-bold'>La partie est finie, Félicitations, vous avez gagné ! </p>";
+            document.querySelector('#dessin').innerHTML = " ";
+            document.querySelector('#message').innerHTML = "";
+            finish = true;
+            //alert('Vous avez gagné la partie !');
+        }
+        console.log('count', count, 'error', error);
+    } catch (e) {
+        console.log('erreur: ', error);
     }
+}
 
-    //message à afficher en fonction des coups et des erreurs    
-    if ((10 - count) === 1) {
-        document.querySelector('#messageRound').textContent = "Il vous reste " + (10 - count) + " coup à jouer";
-    } else {
-        document.querySelector('#messageRound').textContent = "Il vous reste " + (10 - count) + " coups à jouer";
-    };
-
-    if (error === 8 || count === 10) {
-        document.querySelector('#messageRound').innerHTML = "<p class='text-danger fw-bold'>La partie est finie, Vous avez perdu ! Le mot était "+ wordNow.join('').toUpperCase() +" </p>";
-        document.querySelector('#message').innerHTML = "";
-        finish = true;
-        alert('Vous avez perdu la partie');
-    }
-
-    if (tableau.toString() === wordNow.toString()) {
-        document.querySelector('#messageRound').innerHTML = "<p class='text-success fw-bold'>La partie est finie, Félicitations, vous avez gagné ! </p>";
-        document.querySelector('#dessin').innerHTML = " ";
-        document.querySelector('#message').innerHTML = "";
-        finish = true;
-        alert('Vous avez gagné la partie !');
-    }
-    console.log('count', count, 'error', error);
+function lastChance() {
+    
 }
 
 changeWord();
