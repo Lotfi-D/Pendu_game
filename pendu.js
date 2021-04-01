e56caf073966a2e3878235a2ca117591123a4615
Lotfi <lotfi.doucanef@pertimm.com>
2021-04-01 11:23:18 +0200

    #gf1dpe [UPDATE] the words for the game has been updated

--- pendu.js
+++ pendu.js
@@ -1,20 +1,23 @@
 let words = [
-    'fort', 'ingénieux', 'determine', 'shonen', 'enveloppe', 'poste', 'yzutzzpo'
-];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    'fort', 'ingénieux', 'determine', 'shonen', 'enveloppe', 'poste', 'Aquarium', 'Araignée', 'Arbalète', 'Archipel', 'Banquise', 'Batterie', 'Brocante',
+    'Brouhaha', 'Capeline', 'Clavecin', 'Cloporte', 'Débutant', 'Diapason', 'Gangster', 'Gothique', 'Hautbois', 'Hérisson', 'Logiciel', 'Objectif',
+    'Paranoïa', 'Parcours', 'Pastiche', 'Question', 'Quetsche', 'Scarabée', 'Scorpion', 'Symptôme', 'Tabouret', 'Tomahawk', 'Toujours', 'Tourisme',
+    'Triangle', 'Utopique', 'Zeppelin', 'Accordéon', 'Ascenseur', 'Ascension', 'Aseptiser', 'Autoroute', 'Avalanche', 'Balalaïka', 'Bilboquet', 'Bourricot',
+    'Brillance', 'Cabriolet', 'Contrario', 'Cornemuse', 'Dangereux', 'Épluchage', 'Féodalité', 'Forteresse', 'Gondolier', 'Graphique', 'Horoscope', 'Intrépide',
+    'Klaxonner', 'Mascarade', 'Métaphore', 'Narrateur', 'Péripétie', 'Populaire', 'Printemps', 'Quémander', 'Tambourin', 'Vestiaire', 'Xylophone', 'Acrostiche', 
+    'Apocalypse','Attraction', 'Aventurier', 'Bouillotte', 'Citrouille', 'Controverse', 'Coquelicot', 'Dissimuler', 'Flibustier', 'Forestière', 'Grenouille',
+    'Impossible', 'Labyrinthe', 'Maharadjah', 'Prudemment', 'Quadriceps', 'Soliloquer', 'Subjective', 'Abriter', 'Ballast', 'Baryton', 'Bassine', 'Batavia',
+    'Billard', 'Bretzel', 'Cithare', 'Chariot', 'Clairon', 'Corbeau', 'Cortège', 'Crapaud', 'Cymbale', 'Dentier', 'Djembé', 'Drapeau', 'Exemple',
+    'Fourmis', 'Grandir', 'Iceberg', 'Javelot', 'Jockey', 'Journal', 'Journée', 'Jouxter', 'Losange', 'Macadam', 'Mondial', 'Notable', 'Oxygène',
+    'Panique', 'Pétrole', 'Poterie', 'Pouvoir', 'Renégat', 'Scooter', 'Senteur', 'Sifflet', 'Spirale', 'Sucette', 'Strophe', 'Tonneau', 'Trousse',
+    'Tunique', 'Ukulélé', 'Vautour', 'Zozoter', 'Acajou', 'Agneau', 'Alarme', 'Ananas', 'Angora', 'Animal', 'Arcade', 'Aviron', 'Azimut', 'Babine',
+    'Balade', 'Bonzaï', 'Basson', 'Billet', 'Bouche', 'Boucle', 'Bronze', 'Cabane', 'Caïman', 'Cloche', 'Chèque', 'Cirage', 'Coccyx', 'Crayon', 'Garage',
+    'Gospel', 'Goulot', 'Gramme', 'Grelot', 'Guenon', 'Hochet', 'Hormis', 'Humour', 'Hurler', 'Jargon', 'Limite', 'Lionne', 'Menthe', 'Oiseau', 'Podium',
+    'Poulpe', 'Poumon', 'Puzzle', 'Quartz', 'Rapide', 'Séisme', 'Tétine', 'Tomate', 'Walabi', 'Whisky', 'Zipper', 'Accès', 'Aimer', 'Aloès', 'Assez', 'Avion', 
+    'Awalé', 'Balai', 'Banjo', 'Barbe', 'Bonne', 'Bruit', 'Buche', 'Cache', 'Capot', 'Carte', 'Chien', 'Crâne', 'Cycle', 'Ébène', 'Essai', 'Gifle', 'Honni', 'Jambe', 
+    'Koala', 'Livre', 'Lourd', 'Maman', 'Moult', 'Noeud', 'Ortie', 'Pêche', 'Poire', 'Pomme', 'Poste', 'Prune', 'Radar', 'Radis', 'Robot', 'Route', 'Rugby', 'Seuil', 
+    'Taupe', 'Tenue', 'Texte', 'Tyran', 'Usuel', 'Valse',
+    ];
 
 let wordNow = '';
 let tableau = [];
